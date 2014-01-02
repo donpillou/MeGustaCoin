3478b25ea03ecef7c8627661cc68d170381227db
Donald Pillou <don.pillou@gmail.com>
2014-01-02 23:44:55 +0100

    Added BtcChina connection error message

--- Src/Markets/BtcChinaMarketStream.cpp
+++ Src/Markets/BtcChinaMarketStream.cpp
@@ -12,7 +12,7 @@ void BtcChinaMarketStream::process(Callback& callback)
 
   callback.information("Connecting to BtcChina/CNY...");
 
-  while(!canceled)
+  for(; !canceled; sleep(14))
   {
     QString url = lastTid == 0 ? QString("https://data.btcchina.com/data/historydata") :
       QString("https://data.btcchina.com/data/historydata?since=%1").arg(lastTid);
@@ -22,9 +22,9 @@ void BtcChinaMarketStream::process(Callback& callback)
     if(!result)
     {
       if(connected)
-        callback.error("Lost connection to BtcChina/CNY.");
+        callback.error(QString("Lost connection to BtcChina/CNY: %1").arg(httpRequest.getLastError()));
       else
-        callback.error("Could not connect to BtcChina/CNY.");
+        callback.error(QString("Could not connect to BtcChina/CNY: %1").arg(httpRequest.getLastError()));
       return;
     }
     if(!connected)
@@ -35,39 +35,38 @@ void BtcChinaMarketStream::process(Callback& callback)
     if(canceled)
       break;
 
-    if(!buffer.isEmpty())
-    {
-      // [{"date":"1388417186","price":4406.41,"amount":0.016,"tid":"4144642","type":"sell"},{"date":"1388417186","price":4401,"amount":0.973,"tid":"4144643","type":"sell"}]
+    if(buffer.isEmpty())
+      continue;
 
-      qint64 now = QDateTime::currentDateTimeUtc().toTime_t();
-      QVariantList varList = Json::parse(buffer).toList();
-      if(!varList.isEmpty())
+    // [{"date":"1388417186","price":4406.41,"amount":0.016,"tid":"4144642","type":"sell"},{"date":"1388417186","price":4401,"amount":0.973,"tid":"4144643","type":"sell"}]
+
+    qint64 now = QDateTime::currentDateTimeUtc().toTime_t();
+    QVariantList varList = Json::parse(buffer).toList();
+    if(!varList.isEmpty())
+    {
+      QVariantMap tradeData = varList.back().toMap();
+      quint64 date = tradeData["date"].toULongLong();
+      qint64 offset = now - date;
+      if(offset < timeOffset || !timeOffsetSet)
       {
-        QVariantMap tradeData = varList.back().toMap();
-        quint64 date = tradeData["date"].toULongLong();
-        qint64 offset = now - date;
-        if(offset < timeOffset || !timeOffsetSet)
-        {
-          timeOffset = offset;
-          timeOffsetSet = true;
-        }
-      }
-      foreach(const QVariant& var, varList)
-      {
-        QVariantMap tradeData = var.toMap();
-        MarketStream::Trade trade;
-        trade.date = tradeData["date"].toULongLong() + timeOffset;
-        trade.price = tradeData["price"].toDouble();
-        trade.amount = tradeData["amount"].toDouble();
-        quint64 tid = tradeData["tid"].toULongLong();
-        if(tid > lastTid)
-        {
-          callback.receivedTrade(trade);
-          lastTid = tid;
-        }
+        timeOffset = offset;
+        timeOffsetSet = true;
       }
     }
-    sleep(14);
+    foreach(const QVariant& var, varList)
+    {
+      QVariantMap tradeData = var.toMap();
+      MarketStream::Trade trade;
+      trade.date = tradeData["date"].toULongLong() + timeOffset;
+      trade.price = tradeData["price"].toDouble();
+      trade.amount = tradeData["amount"].toDouble();
+      quint64 tid = tradeData["tid"].toULongLong();
+      if(tid > lastTid)
+      {
+        callback.receivedTrade(trade);
+        lastTid = tid;
+      }
+    }
   }
 
   callback.information("Closed connection to BtcChina/CNY.");
