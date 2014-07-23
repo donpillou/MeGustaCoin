55f8528dc32935464835d3bfad37792d0b2596aa
Donald Pillou <don.pillou@gmail.com>
2014-07-23 20:20:34 +0200

    Changed close button icon

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -5,6 +5,8 @@ int main(int argc, char **argv)
 {
   QApplication app (argc, argv);
 
+  app.setStyleSheet("QTabBar::close-button {background-image: url(\":/Icons/close_cold.png\");} QTabBar::close-button:hover {background-image: url(\":/Icons/close_hot.png\");}");
+
   MainWindow mainWindow;
   mainWindow.show();
 
