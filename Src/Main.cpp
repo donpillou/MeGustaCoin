4fd71fda0008e3abb5a7780178c69eac08b64625
Donald Pillou <don.pillou@gmail.com>
2014-07-27 20:11:16 +0200

    Do not customize tab bar close button on Linux

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -5,7 +5,9 @@ int main(int argc, char **argv)
 {
   QApplication app (argc, argv);
 
+#ifdef _WIN32
   app.setStyleSheet("QTabBar::close-button {background-image: url(\":/Icons/close_cold.png\");} QTabBar::close-button:hover {background-image: url(\":/Icons/close_hot.png\");}");
+#endif
 
   MainWindow mainWindow;
   mainWindow.show();
