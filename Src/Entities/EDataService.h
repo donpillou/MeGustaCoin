78ab853a622e57bd0cf589d0c58124c266787d9e
Donald Pillou <don.pillou@gmail.com>
2015-10-01 08:29:17 +0200

    Fixed initialization of EDataService

--- Src/Entities/EDataService.h
+++ Src/Entities/EDataService.h
@@ -15,7 +15,7 @@ public:
   };
 
 public:
-  EDataService() : Entity(eType, 0), state(State::offline) {}
+  EDataService() : Entity(eType, 0), state(State::offline), selectedSessionId(0), selectedBrokerId(0), loadingMarketOrders(false), loadingMarketTransactions(false) {}
 
   State getState() const {return state;}
   void setState(State state) {this->state = state;}
